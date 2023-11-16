74f83e4d18d88e8ff38ffbad0253d387a8840b12
Matheus William <matheus.william1000@gmail.com>
2023-11-16 12:01:47 -0300

    atualizações e ajustes (login, cadastro, temas...)

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,34 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-import Cadastro from "./paginas/cadastro/Cadastro";
-import Footer from "./paginas/components/footer/Footer";
-import Navbar from "./paginas/components/navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 import Home from "./paginas/home/Home";
 import Login from "./paginas/login/Login";
+import Footer from "./paginas/components/footer/Footer";
+import Navbar from "./paginas/components/navbar/Navbar";
+import Cadastro from "./paginas/cadastro/Cadastro";
+import ListaTemas from "./paginas/components/temas/listaTemas/ListaTemas";
+import FormularioTema from "./paginas/components/temas/formularioTema/FormularioTema";
+
 
 function App() {
   return (
-    <>
+
+    <AuthProvider>
       <BrowserRouter>
         <Navbar />
-          <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/cadastro" element={<Cadastro />} />
-            </Routes>
-          </div>  
+        <div className='min-h-[80vh]'>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/temas" element={<ListaTemas />} />
+            <Route path="/cadastroTema" element={<FormularioTema />} />
+            <Route path="/editarTema/:id" element={<FormularioTema />} />
+          </Routes>
+        </div>
         <Footer />
       </BrowserRouter>
-    </>
+    </AuthProvider>
   );
 }
 
